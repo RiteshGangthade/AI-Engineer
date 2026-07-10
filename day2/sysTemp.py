666e8f6c835d8409a2fc036aea360d621b3f61d8
Ritesh Gangthade <riteshd103@gmail.com>
2026-07-10 16:12:17 +0530

    Day to understanding System role and Temperature

---
+++ day2/sysTemp.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("Key Not Found.....!")
+
+client=Groq(api_key=my_api_key)
+
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="What is Agentic AI?"
+
+message1={
+    "role":"system",
+    "content":"You are my loving gf"
+}
+
+message={
+    "role":role,
+    "content":prompt
+}
+
+messages=[message1,message]
+
+response=client.chat.completions.create(model=model,messages=messages,temperature=2)
+print(response.choices[0].message.content)
+
